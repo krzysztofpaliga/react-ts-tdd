8c740f6215df8404fff21794cfcca3e481efea83
krzysztofpaliga <paligakrzychu@gmail.com>
2024-11-06 20:45:43 +0100

    Adds adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
+import { useState } from 'react';
+
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Hello World!</div>;
+  const [taskName, setTaskName] = useState('');
+
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: 'Learn React',
+      isCompleted: true,
+    },
+  ]);
+
+  const onAddTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: taskName,
+        isCompleted: false,
+      },
+    ]);
+    console.log('Add', taskName);
+  };
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <label htmlFor="task-input">Add Task: </label>
+      <input
+        id="task-input"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button onClick={onAddTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
